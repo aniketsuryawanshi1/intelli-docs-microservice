63f79e724676bfe4d02bc143d0d8fb9c49a6e8e7
Aniket Suryawanshi <159512397+aniketsuryawanshi1@users.noreply.github.com>
2025-05-23 16:25:26 +0530

    Register and login page design added.

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+import LandingPage from "./LandingPage";
+import LoginPage from "./Authentication/LoginPage";
+import Register from "./Authentication/Register";
+
+export { LandingPage, LoginPage, Register };
